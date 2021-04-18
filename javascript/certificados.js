1aa875eed16b6fe85a6f80ad4606fd79ab2aa54c
Vini Antunes <57882903+ViniViniAntunes@users.noreply.github.com>
2021-04-18 00:20:48 -0300

    First commit

---
+++ javascript/certificados.js
@@ -0,0 +1,29 @@
+function adiciona_icone(titulo_curso, link_certificado, link_icone) {
+    var div_icones = document.getElementById('icones_cursos')
+    novo_icone = ""
+    novo_icone += "<span data-tooltip='" + titulo_curso + "'>"
+    novo_icone += "<a href='" + link_certificado + "' alt='" + titulo_curso + "'>"
+    novo_icone += "<img src='" + link_icone + "'></a>"
+    novo_icone += "</span>"
+    div_icones.innerHTML = div_icones.innerHTML + novo_icone
+}
+
+async function getData() {
+    const response = await fetch('https://raw.githubusercontent.com/ViniViniAntunes/vini-antunes-personal-page/master/javascript/data_certificates_final.csv');
+    const data = await response.text();
+    
+    var allRows = data.split(/\r?\n|\r/).slice(1);
+    allRows.length = allRows.length - 1;
+
+    allRows.forEach(elem => {
+        if (elem == null) {
+            return
+        }
+        const row = elem.split(';');
+        adiciona_icone(titulo_curso=row[0],
+                       link_certificado=row[7],
+                       link_icone=row[4]);
+    });
+}
+
+getData();
